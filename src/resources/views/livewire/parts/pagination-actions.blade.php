10e1119bdb26fa848c5163ca7dcbdabbdb02efee
beartropy <beartropy@gmail.com>
2026-02-16 02:09:42 -0300

    Add cursor-pointer to pagination buttons
    Clickable prev/next and page number buttons now show a pointer cursor,
    matching user expectations for interactive elements.

--- src/resources/views/livewire/parts/pagination-actions.blade.php
+++ src/resources/views/livewire/parts/pagination-actions.blade.php
@@ -9,7 +9,7 @@
                     </svg>
                 </span>
             @else
-                <button type="button" wire:click="previousPage" wire:loading.attr="disabled" class="relative inline-flex items-center px-4 py-2 text-sm font-medium border leading-5 rounded-md focus:outline-none focus:ring transition ease-in-out duration-150 {{ $themeConfig['dropdowns']['bg'] }} {{ $themeConfig['dropdowns']['border'] }} {{ $themeConfig['dropdowns']['text'] }} {{ $themeConfig['dropdowns']['hover_text'] }} {{ $themeConfig['dropdowns']['active_bg'] }} {{ $themeConfig['general']['focus_ring'] }}">
+                <button type="button" wire:click="previousPage" wire:loading.attr="disabled" class="relative inline-flex items-center px-4 py-2 text-sm font-medium border leading-5 rounded-md cursor-pointer focus:outline-none focus:ring transition ease-in-out duration-150 {{ $themeConfig['dropdowns']['bg'] }} {{ $themeConfig['dropdowns']['border'] }} {{ $themeConfig['dropdowns']['text'] }} {{ $themeConfig['dropdowns']['hover_text'] }} {{ $themeConfig['dropdowns']['active_bg'] }} {{ $themeConfig['general']['focus_ring'] }}">
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-5 h-5">
                         <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 19.5L8.25 12l7.5-7.5" />
                     </svg>
@@ -18,7 +18,7 @@
 
             {{-- BOTÓN SIGUIENTE --}}
             @if ($paginator->hasMorePages())
-                <button type="button" wire:click="nextPage" wire:loading.attr="disabled" class="relative inline-flex items-center px-4 py-2 ml-3 text-sm font-medium border leading-5 rounded-md focus:outline-none focus:ring transition ease-in-out duration-150 {{ $themeConfig['dropdowns']['bg'] }} {{ $themeConfig['dropdowns']['border'] }} {{ $themeConfig['dropdowns']['text'] }} {{ $themeConfig['dropdowns']['hover_text'] }} {{ $themeConfig['dropdowns']['active_bg'] }} {{ $themeConfig['general']['focus_ring'] }}">
+                <button type="button" wire:click="nextPage" wire:loading.attr="disabled" class="relative inline-flex items-center px-4 py-2 ml-3 text-sm font-medium border leading-5 rounded-md cursor-pointer focus:outline-none focus:ring transition ease-in-out duration-150 {{ $themeConfig['dropdowns']['bg'] }} {{ $themeConfig['dropdowns']['border'] }} {{ $themeConfig['dropdowns']['text'] }} {{ $themeConfig['dropdowns']['hover_text'] }} {{ $themeConfig['dropdowns']['active_bg'] }} {{ $themeConfig['general']['focus_ring'] }}">
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-5 h-5">
                         <path stroke-linecap="round" stroke-linejoin="round" d="M8.25 4.5l7.5 7.5-7.5 7.5" />
                     </svg>
@@ -45,7 +45,7 @@
                             </span>
                         </span>
                     @else
-                        <button type="button" wire:click="previousPage" rel="prev" class="relative inline-flex items-center px-2 py-2 text-sm font-medium border rounded-l-md focus:z-10 focus:outline-none focus:ring-1 {{ $themeConfig['pagination']['text'] }} {{ $themeConfig['dropdowns']['bg'] }} {{ $themeConfig['dropdowns']['border'] }} {{ $themeConfig['dropdowns']['hover_bg'] }} {{ $themeConfig['general']['focus_ring'] }}" aria-label="{{ __('pagination.previous') }}">
+                        <button type="button" wire:click="previousPage" rel="prev" class="relative inline-flex items-center px-2 py-2 text-sm font-medium border rounded-l-md cursor-pointer focus:z-10 focus:outline-none focus:ring-1 {{ $themeConfig['pagination']['text'] }} {{ $themeConfig['dropdowns']['bg'] }} {{ $themeConfig['dropdowns']['border'] }} {{ $themeConfig['dropdowns']['hover_bg'] }} {{ $themeConfig['general']['focus_ring'] }}" aria-label="{{ __('pagination.previous') }}">
                             <svg class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20">
                                 <path fill-rule="evenodd" d="M12.707 5.293a1 1 0 010 1.414L9.414 10l3.293 3.293a1 1 0 01-1.414 1.414l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 0z" clip-rule="evenodd" />
                             </svg>
@@ -69,7 +69,7 @@
                                         <span class="relative inline-flex items-center px-4 py-2 -ml-px text-sm font-medium border cursor-default z-10 {{ $themeConfig['dropdowns']['border'] }} {{ $themeConfig['dropdowns']['active_bg'] }} {{ $themeConfig['general']['title'] }}">{{ $page }}</span>
                                     </span>
                                 @else
-                                    <button type="button" wire:click="gotoPage({{ $page }})" class="relative inline-flex items-center px-4 py-2 -ml-px text-sm font-medium border focus:z-10 focus:outline-none focus:ring-1 {{ $themeConfig['pagination']['text'] }} {{ $themeConfig['dropdowns']['bg'] }} {{ $themeConfig['dropdowns']['border'] }} {{ $themeConfig['dropdowns']['hover_bg'] }} {{ $themeConfig['general']['focus_ring'] }}" aria-label="{{ __('Go to page :page', ['page' => $page]) }}">
+                                    <button type="button" wire:click="gotoPage({{ $page }})" class="relative inline-flex items-center px-4 py-2 -ml-px text-sm font-medium border cursor-pointer focus:z-10 focus:outline-none focus:ring-1 {{ $themeConfig['pagination']['text'] }} {{ $themeConfig['dropdowns']['bg'] }} {{ $themeConfig['dropdowns']['border'] }} {{ $themeConfig['dropdowns']['hover_bg'] }} {{ $themeConfig['general']['focus_ring'] }}" aria-label="{{ __('Go to page :page', ['page' => $page]) }}">
                                         {{ $page }}
                                     </button>
                                 @endif
@@ -79,7 +79,7 @@
 
                     {{-- Next Page Link --}}
                     @if ($paginator->hasMorePages())
-                        <button type="button" wire:click="nextPage" rel="next" class="relative inline-flex items-center px-2 py-2 -ml-px text-sm font-medium border rounded-r-md focus:z-10 focus:outline-none focus:ring-1 {{ $themeConfig['pagination']['text'] }} {{ $themeConfig['dropdowns']['bg'] }} {{ $themeConfig['dropdowns']['border'] }} {{ $themeConfig['dropdowns']['hover_bg'] }} {{ $themeConfig['general']['focus_ring'] }}" aria-label="{{ __('pagination.next') }}">
+                        <button type="button" wire:click="nextPage" rel="next" class="relative inline-flex items-center px-2 py-2 -ml-px text-sm font-medium border rounded-r-md cursor-pointer focus:z-10 focus:outline-none focus:ring-1 {{ $themeConfig['pagination']['text'] }} {{ $themeConfig['dropdowns']['bg'] }} {{ $themeConfig['dropdowns']['border'] }} {{ $themeConfig['dropdowns']['hover_bg'] }} {{ $themeConfig['general']['focus_ring'] }}" aria-label="{{ __('pagination.next') }}">
                             <svg class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20">
                                 <path fill-rule="evenodd" d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z" clip-rule="evenodd" />
                             </svg>
